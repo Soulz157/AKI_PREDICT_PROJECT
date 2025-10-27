7e7b965d7b48d9f4f521260304b8cf0458c0f667
Phoorich Maliwan <s6604062620026@email.kmutnb.ac.th>
2025-10-27 23:13:09 +0700

    Fix scripts

--- apps/backend/app/main.py
+++ apps/backend/app/main.py
@@ -8,3 +8,8 @@ app = FastAPI()
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
+
+    @app.get("/")
+    async def read_root(param: dict):
+        df = pd.DataFrame([param])
+        return df.to_dict(orient="records")[0]
